886e4d843a0b072bf76902ab1828a0c51e530ac7
Richard Scott <bububooo3@gmail.com>
2026-02-21 21:19:48 -0500

    Update python.lua

--- python.lua
+++ python.lua
@@ -1,11 +1,57 @@
-local self = "lu?a|py?thon"
+local self = [[local self = \[\[?\]\]\n
+\n
+local put = io.write\n
+local real = ""\n
+local cango = 0\n
+\n
+for section in self:gmatch("([^\124]*)") do\n
+  if cango < 2 then cango = cango + 1\n
+  else\n
+      for char in section:gmatch(".") do\n
+        local b = char:byte();\n
+  \n
+        if b == 63 then\n
+          for d in self:gmatch(".") do\n
+            local c = d:byte();\n
+            if c == "\n" then\n
+              real = real..("\\n")\n
+            elseif c == '"' then\n
+              real = real..('\\\"')\n
+            elseif c == "\\" then\n
+              real = real..('\\\\')\n
+            else\n
+              real = real..(d)\n
+            end\n
+        end\n
+      else\n
+        real = real..(char)\n
+      end\n
+    end\n
+  end\n
+end\n
+\n
+print(real)\n|self = "?"\n
+\n
+for char in self.split('|')[1]:\n
+    if ord(char) == 63:\n
+        for c in self:\n
+            if c == '\n':\n
+                print("\\n", end='')\n
+            elif c == '"':\n
+                print("\\\"", end='')\n
+            elif c == '\\':\n
+                print("\\\\", end='')\n
+            else:\n
+                print(f"{c}", end='')\n
+    else:\n
+        print(char, end='')\n]]
 
 local put = io.write
 local real = ""
 local cango = 0
 
-for section in self:gmatch("([^|]*)") do
-  if cango == 0 then cango = 1
+for section in self:gmatch("([^\124]*)") do
+  if cango < 2 then cango = cango + 1
   else
       for char in section:gmatch(".") do
         local b = char:byte();
@@ -14,18 +60,20 @@ for section in self:gmatch("([^|]*)") do
           for d in self:gmatch(".") do
             local c = d:byte();
             if c == "\n" then
-              put("\\n")
+              real = real..("\\n")
             elseif c == '"' then
-              put('\\\"')
+              real = real..('\\\"')
             elseif c == "\\" then
-              put('\\\\')
+              real = real..('\\\\')
             else
-              put(d)
+              real = real..(d)
             end
         end
       else
-        put(char)
+        real = real..(char)
       end
     end
   end
 end
+
+print(real)
