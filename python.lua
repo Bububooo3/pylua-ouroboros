0e807960b71c8d8c14571ae8bed82a2ea21e2259
Richard Scott <bububooo3@gmail.com>
2026-02-21 22:26:56 -0500

    Refactor string processing logic in python.lua

--- python.lua
+++ python.lua
@@ -1,19 +1,21 @@
-local self = [[self = local self = \[\[?\]\]\n
+local self = [[local self = \[\[?\]\]\n
 \n
 local put = io.write\n
 local real = ""\n
 local cango = 0\n
+local i = 0\n
 \n
 for section in self:gmatch("([^\\124]*)") do\n
-  if cango < 2 then cango = cango + 1\n
+  if cango <1 then cango = cango + 1\n
   else\n
       for char in section:gmatch(".") do\n
+        i = i + 1\n
         local b = char:byte();\n
   \n
         if b == 63 then\n
           for d in self:gmatch(".") do\n
             local c = d:byte();\n
-            if c == "\n" then\n
+            if c == "\\n" then\n
               real = real..("\\n")\n
             elseif c == '"' then\n
               real = real..('\\\"')\n
@@ -22,40 +24,47 @@ for section in self:gmatch("([^\\124]*)") do\n
             else\n
               real = real..(d)\n
             end\n
-        end\n
+          end\n
       else\n
-        real = real..(char)\n
+        if (char ~= "\\") and (section:sub(i-1,i-1)~="\\") or (char == "\"") then\n
+          real = real..(char)\n
+        end\n
       end\n
     end\n
   end\n
 end\n
 \n
-print(real)\n|self = \"\"\"?\"\"\"\n
-\n
-for char in self.split(chr(124))[1]:\n
-    if ord(char) == 63:\n
-        for c in self:\n
-            if c == '\n':\n
-                print("\\n", end='')\n
-            elif c == '"':\n
-                print("\\\"", end='')\n
-            elif c == '\\':\n
-                print("\\\\", end='')\n
-            elif c == '\\124':\n
-                print("\\124", end='')\n
-            else:\n
-                print(f"{c}", end='')\n
-    else:\n
-        print(char, end='')\n]]
+print(real)\n|self = \"\"\"?\"\"\"
+
+for char in self.split(chr(124))[0]:
+    if ord(char) == 63:
+        for c in self:
+            if c == '\n':
+                print("\\n", end='')
+            elif c == '"':
+                print("\\\"", end='')
+            elif c == '\\':
+                print("\\\\", end='')
+            elif c == '\\124':
+                print("\\124", end='')
+            elif c == '[':
+                print('\\[', end='')
+            else:
+                print(f"{c}", end='')
+    else:
+        if char == '[': char = '\\['
+        print(char, end='')]]
 
 local put = io.write
 local real = ""
 local cango = 0
+local i = 0
 
 for section in self:gmatch("([^\124]*)") do
   if cango <1 then cango = cango + 1
   else
       for char in section:gmatch(".") do
+        i = i + 1
         local b = char:byte();
   
         if b == 63 then
@@ -72,7 +81,9 @@ for section in self:gmatch("([^\124]*)") do
             end
           end
       else
-        real = real..(char)
+        if (char ~= "\\") and (section:sub(i-1,i-1)~="\\") or (char == "\"") then
+          real = real..(char)
+        end
       end
     end
   end
