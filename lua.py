14c4f6ce3331730765df03bd97d77e5a5426e990
Richard Scott <bububooo3@gmail.com>
2026-02-21 22:25:31 -0500

    Update lua.py

--- lua.py
+++ lua.py
@@ -1,19 +1,21 @@
-self = """self = local self = \[\[?\]\]\n
+self = """local self = [[[?]]\n
 \n
 local put = io.write\n
 local real = ""\n
 local cango = 0\n
+local i = 0\n
 \n
 for section in self:gmatch("([^\\124]*)") do\n
-  if cango < 2 then cango = cango + 1\n
+  if cango <1 then cango = cango + 1\n
   else\n
       for char in section:gmatch(".") do\n
+        i = i + 1\n
         local b = char:byte();\n
   \n
         if b == 63 then\n
           for d in self:gmatch(".") do\n
             local c = d:byte();\n
-            if c == "\n" then\n
+            if c == "\\n" then\n
               real = real..("\\n")\n
             elseif c == '"' then\n
               real = real..('\\\"')\n
@@ -22,31 +24,17 @@ for section in self:gmatch("([^\\124]*)") do\n
             else\n
               real = real..(d)\n
             end\n
-        end\n
+          end\n
       else\n
-        real = real..(char)\n
+        if (char ~= "\\") and (section:sub(i-1,i-1)~="\\") or (char == "\"") then\n
+          real = real..(char)\n
+        end\n
       end\n
     end\n
   end\n
 end\n
 \n
-print(real)\n|self = \"\"\"?\"\"\"\n
-\n
-for char in self.split(chr(124))[1]:\n
-    if ord(char) == 63:\n
-        for c in self:\n
-            if c == '\n':\n
-                print("\\n", end='')\n
-            elif c == '"':\n
-                print("\\\"", end='')\n
-            elif c == '\\':\n
-                print("\\\\", end='')\n
-            elif c == '\\124':\n
-                print("\\124", end='')\n
-            else:\n
-                print(f"{c}", end='')\n
-    else:\n
-        print(char, end='')\n"""
+print(real)\n|self = \"\"\"?\"\"\"
 
 for char in self.split(chr(124))[0]:
     if ord(char) == 63:
@@ -59,7 +47,29 @@ for char in self.split(chr(124))[0]:
                 print("\\\\", end='')
             elif c == '\\124':
                 print("\\124", end='')
+            elif c == '[':
+                print('\\[', end='')
             else:
                 print(f"{c}", end='')
     else:
+        if char == '[': char = '\\['
+        print(char, end='')"""
+
+for char in self.split(chr(124))[0]:
+    if ord(char) == 63:
+        for c in self:
+            if c == '\n':
+                print("\\n", end='')
+            elif c == '"':
+                print("\\\"", end='')
+            elif c == '\\':
+                print("\\\\", end='')
+            elif c == '\\124':
+                print("\\124", end='')
+            elif c == '[':
+                print('\\[', end='')
+            else:
+                print(f"{c}", end='')
+    else:
+        if char == '[': char = '\\['
         print(char, end='')
