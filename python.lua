d67d75af90a0c46ea1a53ab7eaa8ff2d86a2d2cc
Richard Scott <bububooo3@gmail.com>
2026-02-21 20:56:05 -0500

    Update python.lua

--- python.lua
+++ python.lua
@@ -7,31 +7,25 @@ local cango = 0
 for section in self:gmatch("([^|]*)") do
   if cango == 0 then cango = 1
   else
-    for char in section:gmatch(".") do
-      local b = char:byte();
+      for char in section:gmatch(".") do
+        local b = char:byte();
   
-    if b == 63 then
-      for d in self:gmatch(".") do
-        local c = d:byte();
-        if c == "\n" then
-          put("\\n")
-        elseif c == '"' then
-          put('\\\"')
-        elseif c == "\\" then
-          put('\\\\')
-        else
-          put(d)
+        if b == 63 then
+          for d in self:gmatch(".") do
+            local c = d:byte();
+            if c == "\n" then
+              put("\\n")
+            elseif c == '"' then
+              put('\\\"')
+            elseif c == "\\" then
+              put('\\\\')
+            else
+              put(d)
+            end
         end
+      else
+        put(char)
       end
-    elseif b == "\n" then
-      put("\\n")
-    elseif b == '"' then
-      put('\\\"')
-    elseif b == "\\" then
-      put('\\\\')
-    else
-      put(char)
     end
-  end
   end
 end
